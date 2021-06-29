b6dd06e51bb9477a586dfc394b37405140a5b775
alonsog <alonso_03@hotmail.com>
2021-06-29 01:33:04 -0500

    WIP:: Se incluye Modal

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
+
+    // var elems = document.querySelectorAll('.carousel');
+    // var instances = M.Carousel.init(elems, {
+    //   duration: 400,
+    //   indicators: true
+    // });    
+    // instance.next(3);
   });
+
